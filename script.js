1d2de821911d72c4db561454c8f8224ab2f74b03
Gavin Haseltine <gavinhaseltine@gmail.com>
2023-07-15 16:36:52 -0500

    added group logic

--- script.js
+++ script.js
@@ -5,9 +5,38 @@ function onReady() {
 
     $('#submitButton').on('click', handleSubmit)
 
+    $('#tableBody').on('click', '.deleteButton', deleteEmployee)
+
+}
+
+function deleteEmployee(){
+    console.log('Deleted');
+    $(this).parent().parent().remove()
 }
 
 function handleSubmit (event) {
     event.preventDefault();
     console.log('In Handle Submit');
+
+    const firstNameText = $('#firstNameSubmit').val();
+    console.log(firstNameText);
+    const lastNameText = $('#lastNameSubmit').val();
+    console.log(lastNameText);
+    const employeeIdText = $('#employeeIdSubmit').val();
+    console.log(employeeIdText);
+    const jobTitleText = $('#jobTitleSubmit').val();
+    console.log(jobTitleText);
+    const annualSalaryText = $('#annualSalarySubmit').val();
+    console.log(annualSalaryText);
+
+    $('#tableBody').append(`
+     <tr>
+        <th>${firstNameText}</th>
+        <th>${lastNameText}</th>
+        <th>${employeeIdText}</th>
+        <th>${jobTitleText}</th>
+        <th>${annualSalaryText}</th>
+        <td><button class="deleteButton">Delete Employee</button></td>
+      </tr> `)
+
 }
